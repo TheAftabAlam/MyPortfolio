2d6cd5da3d00f10bd4bb7c37c65c701161e6a741
TheAftabAlam <aftabalamsi12@gmail.com>
2022-08-15 23:59:19 +0530

    PortFolio

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+let hamberger=document.querySelector(".hamberger");
+let times=document.querySelector(".times");
+let mobileNav= document.querySelector(".mobile-nav");
+
+
+hamberger.addEventListener('click',function(){
+   
+   mobileNav.classList.add("open");
+   
+})
+
+times.addEventListener('click',function(){
+    
+
+    mobileNav.classList.remove("open");
+})
+
+
+
