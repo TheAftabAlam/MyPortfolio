7621d082fd928424ec4600e0b1583c99dbe64368
TheAftabAlam <aftabalamsi12@gmail.com>
2022-08-18 19:36:10 +0530

    Update

--- JS/script.js
+++ JS/script.js
@@ -14,6 +14,22 @@ times.addEventListener('click',function(){
 
     mobileNav.classList.remove("open");
 })
-
+// document.querySelector("#form").addEventListener("submit",sendEmail());
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "d@gmail.com",
+        Password : "00000000000",
+        To : 'a@gmail.com',
+        From : document.querySelector("#email").value,
+        Subject : "New Contact from Portfolio",
+        Body : "Name: "+document.querySelector("#f_name").value+" "+document.querySelector("#l_name").value
+        +"<br>Email: "+document.querySelector("#email").value
+        +"<br>Phone Number: "+document.querySelector("#phone").value
+        +"<br>Message: "+document.querySelector("#msg").value
+    }).then(
+      message => alert("Message Sent Successfully, Thank You")
+    );
+}
 
 
